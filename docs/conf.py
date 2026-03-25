03d5c29e3ed1a53862e1361fdc3288de5c84986d
Jacques Supcik <jacques.supcik@hefr.ch>
2026-03-25 17:32:25 +0100

    Bump version: 0.3.1 → 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.append(str(Path(__file__).parent / "../examples"))
 project = "WASM Generator"
 copyright = "2026, Jacques Supcik"
 author = "Jacques Supcik"
-release = "0.3.1"
+release = "0.3.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
